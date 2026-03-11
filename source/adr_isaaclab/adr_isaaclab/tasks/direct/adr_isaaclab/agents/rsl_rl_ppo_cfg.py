6bd7fed45412c11f66841447c862069ad331d302
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2026-03-11 18:33:52 +0100

    update architecture and gamma to account for long-horizon tasks

--- source/adr_isaaclab/adr_isaaclab/tasks/direct/adr_isaaclab/agents/rsl_rl_ppo_cfg.py
+++ source/adr_isaaclab/adr_isaaclab/tasks/direct/adr_isaaclab/agents/rsl_rl_ppo_cfg.py
@@ -21,9 +21,9 @@ class PPORunnerCfg(RslRlOnPolicyRunnerCfg):
         init_noise_std=1.0,
         actor_obs_normalization=True,
         critic_obs_normalization=True,
-        actor_hidden_dims=[128, 128, 128],
-        critic_hidden_dims=[128, 128, 128],
-        activation="elu",
+        actor_hidden_dims=[256, 256, 256],
+        critic_hidden_dims=[256, 256, 256],
+        activation="tanh",
     )
     algorithm = RslRlPpoAlgorithmCfg(
         value_loss_coef=1.0,
@@ -34,7 +34,7 @@ class PPORunnerCfg(RslRlOnPolicyRunnerCfg):
         num_mini_batches=4,
         learning_rate=1.0e-3,
         schedule="adaptive",
-        gamma=0.99,
+        gamma=0.995,
         lam=0.95,
         desired_kl=0.01,
         max_grad_norm=1.0,
